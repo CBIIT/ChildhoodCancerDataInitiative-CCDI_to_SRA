f3835381fec868dfbc3e14f4077f417e8d8b96a6
svburke <42675174+svburke@users.noreply.github.com>
2023-01-12 11:37:09 -0600

    Update CCDI_to_SRA.R

--- CCDI_to_SRA.R
+++ CCDI_to_SRA.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-#Childhood Cancer Data Initiative - CCDI_to_SRA.R
+#Childrens Cancer Data Initiative - CCDI_to_SRA.R
 
 #This script will take data from a validated CCDI submission manifest, and create an SRA submission file specifically for a CCDI project.
 
